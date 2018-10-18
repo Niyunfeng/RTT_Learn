a5daba2c27634632132c402d8eb03f6614c77488
Nyf <Yunfeng.Ni@haobro.cn>
2018-10-18 11:45:09 +0800

    Part 11.1 1、解决线程无法切换BUG，在void rt_schedule_remove_thread(struct rt_thread *thread)函数中    rt_list_remove(&(thread->tlist));语句将线程删除

--- rtthread/3.0.3/src/scheduler.c
+++ rtthread/3.0.3/src/scheduler.c
@@ -21,15 +21,14 @@ struct rt_thread *rt_current_thread;
 /* 线程就绪列表 */
 rt_list_t rt_thread_priority_table[RT_THREAD_PRIORITY_MAX];
 
-/* 线程休眠列表 */
-rt_list_t rt_thread_defunct;
-
 /* 当前优先级 */
 rt_uint8_t rt_current_priority;
 
 /* 线程就绪优先级组 */
 rt_uint32_t rt_thread_ready_priority_group;
 
+/* 线程休眠列表 */
+rt_list_t rt_thread_defunct;
 
 /*
 *************************************************************************
@@ -39,76 +38,120 @@ rt_uint32_t rt_thread_ready_priority_group;
 
 /* 初始化系统调度器 */
 void rt_system_scheduler_init(void)
-{
+{	
 #if 0
-	register rt_base_t offset;
+	register rt_base_t offset;	
 
-	/* 线程就绪表初始化 */
+	
+	/* 线程就绪列表初始化 */
 	for (offset = 0; offset < RT_THREAD_PRIORITY_MAX; offset ++)
 	{
-		rt_list_init(&rt_thread_priority_table[offset]);
+			rt_list_init(&rt_thread_priority_table[offset]);
 	}
-
+	
 	/* 初始化当前线程控制块指针 */
 	rt_current_thread = RT_NULL;
 #else
-	register rt_base_t offset;
+    register rt_base_t offset;	
 
+	
 	/* 线程优先级表初始化 */
-	for (offset = 0; offset < RT_THREAD_PRIORITY_MAX; offset++)
+	for (offset = 0; offset < RT_THREAD_PRIORITY_MAX; offset ++)
 	{
-		rt_list_init(&rt_thread_priority_table[offset]);
+			rt_list_init(&rt_thread_priority_table[offset]);
 	}
-
+    
 	/* 初始化当前优先级为空闲线程的优先级 */
-	rt_current_priority = RT_THREAD_PRIORITY_MAX - 1;
-
+    rt_current_priority = RT_THREAD_PRIORITY_MAX - 1;
+    
 	/* 初始化当前线程控制块指针 */
 	rt_current_thread = RT_NULL;
-
-	/* 初始化线程就绪优先级组 */
-	rt_thread_ready_priority_group = 0;
-#endif
+    
+    /* 初始化线程就绪优先级组 */
+    rt_thread_ready_priority_group = 0;
+#endif    
 }
 
 /* 启动系统调度器 */
 void rt_system_scheduler_start(void)
 {
 #if 0
-	register struct  rt_thread *to_thread;
+	register struct rt_thread *to_thread;	
 
 	/* 手动指定第一个运行的线程 */
 	to_thread = rt_list_entry(rt_thread_priority_table[0].next,
-	                          struct rt_thread,
-	                          tlist);
+							  struct rt_thread,
+							  tlist);
 	rt_current_thread = to_thread;
-
-	/* 切换到第一个线程，该函数在context_rvds.S中实现，
-	   在rthw.h声明，用于实现第一次线程切换。
-	   当一个汇编函数在C文件中调用的时候，如果有形参，
-	   则执行的时候会将形参串入到CPU寄存器r0。 */
-
+														
+	/* 切换到第一个线程，该函数在context_rvds.S中实现，在rthw.h声明，
+       用于实现第一次线程切换。当一个汇编函数在C文件中调用的时候，
+       如果有形参，则执行的时候会将形参传人到CPU寄存器r0。*/
 	rt_hw_context_switch_to((rt_uint32_t)&to_thread->sp);
 #else
-	register struct rt_thread *to_thread;
-	register rt_ubase_t highest_ready_priority;
+    register struct rt_thread *to_thread;
+    register rt_ubase_t highest_ready_priority;
+                              
+    /* 获取就绪的最高优先级 */
+    highest_ready_priority = __rt_ffs(rt_thread_ready_priority_group) - 1;
+                              
+    /* 获取将要运行线程的线程控制块 */
+    to_thread = rt_list_entry(rt_thread_priority_table[highest_ready_priority].next,
+                              struct rt_thread,
+                              tlist);
 
-	/* 获取就绪的最高优先级 */
-	highest_ready_priority = __rt_ffs(rt_thread_ready_priority_group) - 1;
+    rt_current_thread = to_thread;
 
-	/* 获取将要运行线程的线程控制块 */
-	to_thread = rt_list_entry(rt_thread_priority_table[highest_ready_priority].next,
-	                          struct rt_thread,
-	                          tlist);
+    /* 切换到新的线程 */
+    rt_hw_context_switch_to((rt_uint32_t)&to_thread->sp);
 
-	rt_current_thread = to_thread;
-
-	/* 切换到新的线程 */
-	rt_hw_context_switch_to((rt_uint32_t)&to_thread->sp);
-
-	/* 永远不会返回 */
-#endif
+    /* 永远不会返回 */                         
+#endif                              
 }
+
+
+void rt_schedule_insert_thread(struct rt_thread *thread)
+{
+    register rt_base_t temp;
+
+    /* 关中断 */
+    temp = rt_hw_interrupt_disable();
+
+    /* 改变线程状态 */
+    thread->stat = RT_THREAD_READY;
+
+    /* 将线程插入就绪列表 */
+    rt_list_insert_before(&(rt_thread_priority_table[thread->current_priority]),
+                          &(thread->tlist));
+
+    /* 设置线程就绪优先级组中对应的位 */
+    rt_thread_ready_priority_group |= thread->number_mask;
+
+    /* 开中断 */
+    rt_hw_interrupt_enable(temp);
+}
+
+void rt_schedule_remove_thread(struct rt_thread *thread)
+{
+	register rt_base_t temp;
+
+	/* 关中断 */
+	temp = rt_hw_interrupt_disable();
+
+	/* 将线程从就绪列表中删除 */
+	rt_list_remove(&(thread->tlist));
+
+	/* 将线程就绪优先级组员的位清除 */
+	if(rt_list_isempty(&(rt_thread_priority_table[thread->current_priority])))
+	{
+		rt_thread_ready_priority_group &= ~thread->number_mask;
+	}	
+
+	/* 开中断 */
+	rt_hw_interrupt_enable(temp);
+}
+
+
 
 /* 系统调度 */
 void rt_schedule(void)
@@ -149,47 +192,4 @@ void rt_schedule(void)
 	}
 
 }
-
-void rt_schedule_insert_thread(struct rt_thread *thread)
-{
-	register rt_base_t temp;
-
-	/* 关中断 */
-	temp = rt_hw_interrupt_disable();
-
-	/* 改变线程状态 */
-	thread->stat = RT_THREAD_READY;
-
-	/* 将线程插入到就绪列表 */
-	rt_list_insert_before(&(rt_thread_priority_table[thread->current_priority]),
-                          &(thread->tlist));
-
-	/* 设置线程就绪优先级组中对应的位 */
-	rt_thread_ready_priority_group |=  thread->number_mask;
-
-	/* 开中断*/
-	rt_hw_interrupt_enable(temp);
-	
-}
-
-void rt_schedule_remove_thread(struct rt_thread *thread)
-{
-	register rt_base_t temp;
-
-	/* 关中断 */
-	temp = rt_hw_interrupt_disable();
-
-	/* 将线程从就绪列表中删除 */
-	rt_list_remove(&(thread->list));
-
-	/* 将线程就绪优先级组员的位清除 */
-	if(rt_list_isempty(&(rt_thread_priority_table[thread->current_priority])))
-	{
-		rt_thread_ready_priority_group &= ~thread->number_mask;
-	}	
-
-	/* 开中断 */
-	rt_hw_interrupt_enable(temp);
-}
-
 
